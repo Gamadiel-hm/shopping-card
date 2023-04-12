b5a5a6bbbf3a143790b6dfd81e109c0c6744a7d1
Gamadiel <gamadiel-hm@hotmail.com>
2023-04-12 13:40:42 -0600

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h2>Shopping card</h2>
+      </header>
+      <main>
+        <p>Products</p>
+      </main>
+    </>
+  );
+}
